7b921b358d817af15ae7a93e5555a141a9244e68
mcostadecarvalho <mcostadecarvalho@itba.edu.ar>
2026-04-08 23:30:49 -0300

    Add files via upload

--- TP_1/eje_5/eje_5.c
+++ TP_1/eje_5/eje_5.c
@@ -1,50 +1,69 @@
+/*
+ * TP 1
+ * Ejercicio 5                                                                           
+ * Escriba un programa que permanentemente solicite un número entero e imprima en pantalla      
+ * el promedio de los últimos 10 números ingresados hasta que el usuario ingrese la             
+ * palabra “EXIT”.
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define n 10
+#define _N 10
 
-void promedioDinamicoUltimos10();
+int main(void){
 
-int main() {
-    //PromedioConOrdenamientoBurbuja();
-    promedioDinamicoUltimos10();
-    
-    return 0;
-}
+    char buffer[50];
+    int i=0;
+    int h=0;
+    int numeros[_N]={0};
+    float promedio;
+    long num;
+    char *fin;
 
-
-//PEDIR NUMEROS CADA UN SEGUNDO AL USUARIO Y CALCULAR EL PROMEDIO DE LOS ULTIMOS 10 NUMEROS INGRESADOS
-//LA CONDICION SE DEBE CUMPLIR HASTA QUE SE INGRESE "EXIT"
-
-void promedioDinamicoUltimos10() {
-    float arr[n], num, sum = 0.0;
-    int count = 0;
-
-    while (1) {
-        printf("Ingrese un numero (o 'EXIT' para salir): ");
-        if (scanf("%f", &num) != 1) { //Verfico que la entrada no es un numero
-            char exitStr[5];
-            scanf("%s", exitStr);
-            if (strcmp(exitStr, "EXIT") == 0) {
-                break; // si efectivamente no es un numero se compara con EXIT, si es igual, se corta el programa
-            } else {
-                printf("Entrada no valida. Intente de nuevo.\n"); 
-                continue;
-            }
+    while(1){
+        printf("Ingrese un numero entero o EXIT para finalizar:\n");
+        if(fgets(buffer, 50, stdin)==NULL){
+            printf("Error de lectura.\n");
+            return 1;
         }
-        if (count + 1 < n ) {
-            arr[count+1] = num;
-            sum += num;
-            count++;
-            printf("Ingrese %d numeros para completar los ultimos 10 y hacer el promedio.\n", n - count);
-        } else {
-            sum -= arr[0]; // Restar el primer numero del arreglo, YA TIENE SUMADOS LOS ANTERIORES
-            for (int i = 0; i < n; i++) {
-                arr[i] = arr[i + 1]; // Desplazar los numeros a la izquierda
-            }
-            arr[n - 1] = num; // Agregar el nuevo numero al final
-            sum += num; // Sumar el nuevo numero
-            printf("El promedio de los ultimos %d numeros es: %.2f\n", 10 , sum / 10);
-        }  
+        buffer[strcspn(buffer, "\n")]='\0';
+        
+        if(strcmp(buffer,"EXIT")==0){
+            printf("Programa terminado\n"); 
+            return(0);
+        }
+
+        num = strtol(buffer, &fin, 10); // (string to long) convierte buffer a numero; fin apunta al primer caracter no convertido; 10 indica base decimal
+
+         if(*buffer == '\0' || *fin != '\0'){  //primer condición: usuario no ingresó nada, segunda: usuario ingresó caracteres inválidos
+            printf("Error: debe ingresar un numero entero valido o la palabra EXIT.\n");
+            continue;
+        }
+
+        numeros[i] = (int)num;
+
+        promedio=0;
+        for(int j=0; j<_N;j++){
+            promedio=promedio+numeros[j];
+
+        }
+    
+        i=i+1;
+        h=h+1;
+
+        if(h<=10){
+            printf("El promedio de los ultimos %d numeros ingresados es: %.2f \n",h , promedio/((float) h));
+        }else{
+            printf("El promedio de los ultimos 10 numeros ingresados es: %.2f \n", promedio/((float) _N));
+        }
+        
+        if(i==_N){
+            i=0;
+        }
+        
     }
+
+    return 0;
 }
